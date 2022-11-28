292492cf1152a012511c253c77c87fae7ea4fbbc
x-farkasle <x-farkasle@wizzair.com>
2022-11-28 23:54:30 +0100

    title update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import movies from './movies.json';
 const App = () => {
   return (
     <div className="App">
-      <h1>The 30 best films of 2022 (so far)</h1>
+      <h1>The 6 best films of 2022 (so far)</h1>
       <div className="wrapper">
         <div className="layout">
           <main className="container">
